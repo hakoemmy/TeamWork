f4591a2582cb07aa1fc0a2e13906622582e8ff85
Emmanuel HAKORIMANA <51243858+hakoemmy@users.noreply.github.com>
2019-09-18 21:02:01 +0200

    feat(comment): create comment on article layout (#13)

---
+++ UI/js/script.js
@@ -0,0 +1,49 @@
+const openNav = () => {
+  document.querySelector('#mySidenav').style.width = '250px';
+}
+
+const closeNav = () => {
+  document.querySelector('#mySidenav').style.width = '0';
+};
+// Tabbed comment panel
+const openTab = (evt, tabName) => {
+  if(tabName === 'preview'){
+    const comment = document.querySelector('#comment');
+    const prev = document.querySelector('#comment-preview');
+   prev.innerHTML=comment.value;
+  }
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Dialog Box
+const dialogbox = (message) => { // Get the modal
+  const modal = document.querySelector('#dialogbox');
+
+  const divMsg = document.querySelector('.dialog-content-js');
+
+
+  divMsg.textContent = message;
+  // Display the modal
+  modal.style.display = 'block';
+};
+const closeDialog = () => {
+  document.querySelector('.modal').style.display = 'none';
+  const modal = document.querySelector('#dialogbox');
+  modal.style.display = 'none';
+};
+
+const shareArticle = () => {
+  document.querySelector('.modal-view').style.display = 'block';
+};
+const closeShareArticle = () => {
+  document.querySelector('.modal-view').style.display = 'none';
+};
