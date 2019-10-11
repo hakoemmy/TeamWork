2733e48465921d48ba01c3f3e601fc565a6b0518
H.Emmanuel <hakoemmy.2017@gmail.com>
2019-10-11 13:33:04 +0200

    chore(LFA-feedback): make one instance of middleware in the server
    [Finishes #169090080]

--- server/index.js
+++ server/index.js
@@ -1,30 +1,12 @@
 import express from 'express';
-import bodyParse from 'body-parser';
 import dotenv from 'dotenv';
-import swaggerUI from 'swagger-ui-express';
+import route from './routes/mainRoute';
 import config from './config/default';
-import userRoute from './routes/userRoute';
-import articleRoute from './routes/articleRoute';
-import isContentTypeValid from './middleware/isContentTypeValid';
-import isValidJson from './middleware/isValidJson';
-import swaggerDoc from '../app.json';
-import { NOT_FOUND } from './helpers/statusCode';
+
 
 dotenv.config();
-
 const app = express();
-app.use(bodyParse.json());
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
-app.use('/api/v2', isValidJson, articleRoute);
-app.use('/api/v2/auth', isContentTypeValid, isValidJson, userRoute);
-
-
-app.use('/', (req, res) => {
-  res.status(NOT_FOUND).send({
-    status: NOT_FOUND,
-    message: 'Welcome to TeamWork API!',
-  });
-});
+app.use('/', route);
 
 const { port } = config;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
